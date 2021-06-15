f835b87414698f93eb457099cfbe857173a18883
Al-Namrood <plesant.benjamin@gmail.com>
2021-06-15 12:33:15 +0200

    Users fixtures problem with password encrypt

--- src/DataFixtures/UsersFixture.php
+++ src/DataFixtures/UsersFixture.php
@@ -2,16 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+
+
 class UsersFixture extends Fixture
 {
+
+//    private $encoder;
+//
+//    public function __construct(UserPasswordEncoderInterface $encoder)
+//    {
+//        $this->encoder = $encoder;
+//    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new Users();
+        $user->setFirstname("Jean miche");
+        $user->setLastname("de la magnagna");
+        $user->setMail("jeanmiche@gmail.com");
 
+
+        $user->setPassword(password_hash('putain', PASSWORD_BCRYPT ));
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
