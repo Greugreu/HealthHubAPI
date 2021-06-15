c69a4f2b1c1280a57408102a40163196e8daf5cd
Al-Namrood <plesant.benjamin@gmail.com>
2021-06-15 16:18:22 +0200

    Create user

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,12 +2,70 @@
 
 namespace App\Controller;
 
+use App\Entity\StatsBio;
+use App\Entity\Users;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends AbstractController
 {
+
+    private EntityManagerInterface $manager;
+
+    public function __construct(EntityManagerInterface $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @Route("create/user", name="createUser", methods={"POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function createUser(Request $request): Response
+    {
+        $firstname = $request->request->get('firstname');
+        $lastName = $request->request->get('lastname');
+        $email = $request->request->get('mail');
+        $password = $request->request->get('password');
+        $gender = $request->request->get('gender');
+
+
+        $response = new Response();
+
+        try {
+            if (!empty($firstname) && !empty($lastName) && !empty($email) && !empty($password) && !empty($gender)) {
+                $user = new Users();
+                $stats = new StatsBio();
+                $user->setFirstname($firstname)
+                    ->setLastname($lastName)
+                    ->setMail($email)
+                    ->setPassword($password)
+                    ->setGender($gender);
+                $user->setStatsBioIdStatsBio($stats);
+                $this->manager->persist($user);
+                $this->manager->flush();
+
+                $response->setStatusCode(201);
+            } else {
+                $response->setStatusCode(400);
+
+
+            }
+        }
+        catch (NotFoundHttpException $e){
+            throw $e;
+        }
+
+        return $response;
+
+
+    }
+
     /**
      * @Route("/users", name="users")
      */
---
+++ src/service/UsersService.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\service;
+
+
+use App\Repository\UsersRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+//class UsersService
+//{
+//    public function createUser(Request $request, UsersRepository $usersRepository): Response
+//    {
+//        $firstname = $request->request->get('firstname');
+//        $lastName = $request->request->get('lastname');
+//        $email = $request->request->get('mail');
+//        $password = $request->request->get('password');
+//        $gender = $request->request->get('gender');
+//        $activity = $request->request->get('activityLog_idActivityLog');
+//
+//        $response
+//    }
+//
+//}
