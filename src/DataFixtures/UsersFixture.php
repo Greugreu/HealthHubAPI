d684afb02267121ca5c37684e4eec7c8fdb547e8
Victor CLARKE <Victor.Clarke@alphamaintenance-sa.com>
2021-06-15 14:20:23 +0200

    fix User fixtures

--- src/DataFixtures/UsersFixture.php
+++ src/DataFixtures/UsersFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\StatsBio;
 use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -21,9 +22,15 @@ class UsersFixture extends Fixture
     public function load(ObjectManager $manager)
     {
         $user = new Users();
+        $statsBio = new StatsBio();
+        $statsBio->setHeight(1.70);
+        $statsBio->setWeight(65);
         $user->setFirstname("Jean miche");
         $user->setLastname("de la magnagna");
         $user->setMail("jeanmiche@gmail.com");
+        $user->setGender(1);
+        $statsBio->setCreatedAt(new \DateTime('now'));
+        $user->setStatsBioIdStatsBio($statsBio);
 
 
         $user->setPassword($this->passwordEncoder->encodePassword($user, 'LePasswordPutain'));
